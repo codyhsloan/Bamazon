9a17f295294fe5220669360ddaf0cd64aff08bd0
Cody Sloan <chsloan@ncsu.edu>
2017-09-21 17:54:09 -0400

    third commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,92 +24,63 @@ connection.connect(function(err) {
 // Display All Items inside Database 
 
 function itemForSale() {
-  connection.query("SELECT * FROM products", function(err, res) {
+	console.log("All Products for Sale \n")
 
-  	 console.log("Product Name  |  Department Name  |   Price  | In Stock");
-     console.log("---------------------------------")
+    var query = connection.query("SELECT * FROM products", function(err, res) {
+    	if (err) { 
+    		throw (error);
+    	};
 
-    for (var i = 0; i < res.length; i++) {
-   
-      console.log(res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
-    }
-    console.log("-----------------------------------");
-  });
+    for (var i = 0; i < response.length - 1; i++) {
+    	console.log("item ID: " + res[i].item_id + "\n  Product" + res[i].product_name + "\n  Department" + res[i].department_name + "\n $" 
+    	+ res[i].price + "\n  Stock" + res[i].stock_quantity);
+    	}
+
+	customerSelection();
+
+	});
 }
 
 function customerSelection() {
-  // query the database for all items being auctioned
-  connection.query("SELECT * FROM products", function(err, results) {
-    if (err) throw err;
-    // once you have the items, prompt the user for which they'd like to purchase
+  // query the database for all items being sold
     inquirer
       .prompt([
         {
-          name: "choice",
-          type: "rawlist",
-          choices: function() {
-            var choiceArray = [];
-            for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].product_name);
-            }
-            return choiceArray;
-          },
-          message: "What item(s) would you like to purchase?"
-        },
-        {
-          name: "purchase",
+          name: "itemid",
           type: "input",
-          message: "How many would you like to buy?"
-        }
-      ])
-      .then(function(answer) {
-        // get the information of the chosen item
-        var chosenItem;
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].product_name === answer.choice) {
-            chosenItem = results[i];
-          }
-        }
-        // determine if there is enough supply to fill customer order
-        if (chosenItem.stock_quantity >= parseInt(answer.choice)) {
-          // purchase was successful, so update db, let the user know, and start over
-          connection.query(
-            "UPDATE auctions SET ? WHERE ?",
-            [
-              {
-                stock_quantity: answer.choice
-              },
-              {
-                id: chosenItem.id
-              }
-            ],
-            function(error) {
-              if (error) throw err;
-              console.log("Purchase successful!");
-              
+          message: "Enter the ID number associated with the product you would like to purchase:"
+        },{
+          name: "quantity"
+          type: "input"
+          message: "How many item(s) would you like to buy?"
+        }]).then(function(answer) {
+        	connection.query("SELECT * FROM products WHERE ?", {item_id: answer.itemid} function(err, results) {
+              if (res[0].stock_quantity < answer.quantity) {
+              	console.log("Insufficient Supply of that Product");
+              }else {
+              	var stockLeftOver = res[0].stock_quantity - answer.quantity;
+
+              	console.log("You purchased " + answer.quantity + "of" + res[0].product_name + ".")
+
+              	console.log("Your total is: $" + answer.quantity * res[0].price + ".")
+
+              	connection.query("UPDATE.products SET ? WHERE ?", [
+              	{
+              		stock_quantity: stockLeftOver
+              	},
+              	{
+              		item_id: answer.itemid
+              	}
+
+              	}
+              ], function(e, re) {
+              	console.log(re,affectedRows + "product(s) updated!");
+              	console.log(res[0].stock_quantity - answer.quantity + "left in stock")
+              	connection.end();
+
+               });
             }
-          );
-        }
-        else {
-          // purchase amount too high, so apologize and start over
-          console.log("Insufficient Supply!");
-          
-        }
-        // Update mySQL database with reduced inventory
-            var newInventory = parseInt(stock_quantity) - parseInt(answer.choice); 
-            connection.query('UPDATE Products SET ? WHERE ?', [{stock_quantity: newInventory}], function(err, res){
-              if(err) throw err; // Error Handler
-            }); 
-
-             // Show customer their purchase total (need to query the price info from database)
-            var customerTotal;
-            connection.query('SELECT Price FROM Products WHERE ?', function(err, res){
-              
-              var price = res[0].price;
-              customerTotal = stock_quantity*price.toFixed(2);
-
-              console.log('\nYour total is $' + customerTotal + '.');
-      		});
-  		});
-	}
+        });
+    });
 }
+
